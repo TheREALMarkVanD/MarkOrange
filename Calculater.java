bde88f24039349a63a6fdd9f9b43f7530e5dfdba
TheREALMarkVanD <91272753+TheREALMarkVanD@users.noreply.github.com>
2024-09-06 11:37:43 +0000

    Update v2 Calculater.java

--- Calculater.java
+++ Calculater.java
@@ -6,10 +6,13 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
-
+    public int mult(int a, int b) {
+        return a * b;
+    }
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("10 + 5 = " + calc.add(10, 5));
         System.out.println("10 - 5 = " + calc.subtract(10, 5));
+        System.out.println("10 * 5 = " + calc.mult(10, 5));
     }
 }
